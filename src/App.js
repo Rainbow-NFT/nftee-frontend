0d1671c94493be717bbcd01d2d03897b9b7c3769
e <abrandecarlo@protonmail.com>
2022-04-24 23:31:17 -0700

    ✨ requests 18x more API calls than neccesary ✨

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState} from "react";
 import {ethers} from "ethers";
 
+// CURL Imports
+import request from 'request'
+
 // ABI Imports
-import WowNFT from "./abi/WowNFT.json"
+import SvgBitpack from "./abi/SvgBitpack.json"
 
 // Styling & Logos
 import './styles/App.css';
@@ -10,32 +13,49 @@ import twitterLogo from './assets/twitter-logo.svg';
 import opensealogo from './assets/opensea-logo.svg';
 import rariblelogo from './assets/rarible-logo.svg';
 import etherscanlogo from './assets/etherscan-logo.svg';
-
+import githublogo from './assets/github-logo-white.svg';
 
 /// ============ Constants ============
-// Rinkeby Address
-const CONTRACT_ADDRESS = "0xdCB96551E07E719C300bBE4d045358DeB9897baf";
 
-// Links
+// Rinkeby Address
+const CONTRACT_ADDRESS = "0xce4346e22dD8288D2971416d29E99DB22385E0A4";
+
+/// Links ///
+
+// Social Media
 const TWITTER_HANDLE = 'abran_decarlo';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-// Asset Marketplace Links
-const OPENSEA_COLLECTION_LINK = `https://testnets.opensea.io/assets/wow-genbkasdgk`;
+// Asset Links
+const OPENSEA_COLLECTION_LINK = `https://testnets.opensea.io/assets/rainbow-nft`;
 const RARIBLE_COLLECTION_LINK = `https://rinkeby.rarible.com/collection/${CONTRACT_ADDRESS}/items`;
 const ETHERSCAN_LINK = `https://rinkeby.etherscan.io/address/${CONTRACT_ADDRESS}`;
 
-// Counters
-const TOTAL_MINT_COUNT = 50;
+// Github Org Repo
+const GITHUB_REPO =  `https://github.com/Rainbow-NFT`;
+
+// Token Cap
+var TOTAL_TOKEN_AMOUNT = Number(10000);
 
 /// ============ Main App ============
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [tokenId, setTokenId] = React.useState("");
+  /// ============ Fetch Tokens Left ============
 
-  /// ============ Wallet Stuff ============
+  useState((async () =>{
+    request('https://secure-shore-07844.herokuapp.com/currentTokenId', function (error, response, body) {
+      console.log(body); // Print the HTML for the Google homepage.
+      setTokenId(Number(body));
+    });
+    }
+  )()); 
 
-  // Checks if the browser has Metamask
+  
+   /// ============ Connect Wallet ============
+
+    // Checks if the browser has Metamask
   const checkIfWalletIsConnected = () => {
     const {ethereum} = window;
     if (!ethereum) {
@@ -44,9 +64,8 @@ const App = () => {
   } else {
     console.log("🦊 Metamask found", ethereum);
   }
-}
+  }
 
-  // Connect wallet
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -55,41 +74,34 @@ const App = () => {
         alert("Get 🦊 Metamask!");
         return;
       }
-
       const accounts = await ethereum.request({ method: "eth_requestAccounts"});
-    
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
 
-      setupEventListener()
+      /* setupEventListener() */
     } catch (error) {
       console.log(error);
     }
   }
 
-    /// ============ Mint NFT ============
+
+  /// ============ Mint NFT ============
 
   const mintNFT = async () => {
-    // Rinkeby Address
-  
-    // We try to mint
     try {
         const { ethereum } = window;
-
         if (ethereum) {
             const provider = new ethers.providers.Web3Provider(ethereum);
             const signer = provider.getSigner();
-            const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, WowNFT.abi, signer);
-        
+            const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, SvgBitpack.abi, signer);
             console.log("Opening wallet to confirm transaction");
-            let nftTxn = await connectedContract.makeAWowNFT();
-
-
+            let nftTxn = await connectedContract.mintTo(signer.getAddress());
             console.log("⛏️ Mining in progress... please wait");
             await nftTxn.wait();
-
             console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
-
+            const _tokenId = await connectedContract.currentTokenId
+            setTokenId(Number(10000) - Number(_tokenId));
+           
         } else {
             console.log("Ethereum object doesn't exist!");
         }
@@ -99,51 +111,25 @@ const App = () => {
 
   }
 
-  /// ============ Event Listeners ============
-  const setupEventListener = async () => {
-
-    try {
-      const { ethereum } = window;
-      
-      if (ethereum) {
-        // can we make it so we don't have to copy & paste the same code again?
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, WowNFT.abi, signer);
-
-        connectedContract.on("NewNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`NFT minted and it has been sent to your wallet.  It may take up to 15 min for it to show up on OpenSea.`);
-          alert(`https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
-        }); 
-
-        console.log("Event listener setup");
-      } else {
-        console.log("Ethereum object doesn't exist");
-      } 
-      } catch (error) {
-        console.log(error)
-    } 
-  }
-
   /// ============ Render Stuff ============
 
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
 
+  useState()
+
   // Render Methods
   const renderNotConnectedContainer = () => (
   <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>);
-
+  
   const renderMintUI = () => (
     <button onClick={mintNFT} className="cta-button connect-wallet-button">
       Mint NFT
     </button>
   )
-
 
   /// ============ Return Page ============
 
@@ -151,14 +137,15 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Groovy NFTs</p>
+          <p className="header gradient-text">Rainbow NFTs</p>
           <p className="sub-text">
-            Each uniquely generated, completely on chain.
+            Each uniquely generated, bitpacked on-chain.
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : renderMintUI()}
         </div>
+        <p className="sub-text">{tokenId}/{TOTAL_TOKEN_AMOUNT} NFTs left</p>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
@@ -191,6 +178,18 @@ const App = () => {
             target="_blank"
             rel="noreferrer"
           >{<img alt="Etherscan Logo" className="etherscan-logo" src={etherscanlogo} />}</a>
+           <a
+            className="footer-text"
+            target="_blank"
+            rel="noreferrer"
+            // This is why I don't do much frontend
+          >&nbsp;&nbsp;&nbsp;&nbsp;Repo:&nbsp;&nbsp;</a>
+           <a
+            className="footer-text"
+            href={GITHUB_REPO}
+            target="_blank"
+            rel="noreferrer"
+          >{<img alt="Github Logo" className="github-logo" src={githublogo} />}</a>
         </div>
       </div>
     </div>
