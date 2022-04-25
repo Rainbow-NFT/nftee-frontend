2da77e30d3640f70d7b51d7ec162bde55385ef82
e <abrandecarlo@protonmail.com>
2022-04-25 14:31:51 -0700

    lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState} from "react";
-import {ethers} from "ethers";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 // CURL Imports
-import request from 'request'
+import request from 'request';
 
 // ABI Imports
-import SvgBitpack from "./abi/SvgBitpack.json"
+import SvgBitpack from './abi/SvgBitpack.json';
 
 // Styling & Logos
 import './styles/App.css';
@@ -18,7 +18,7 @@ import githublogo from './assets/github-logo-white.svg';
 /// ============ Constants ============
 
 // Rinkeby Address
-const CONTRACT_ADDRESS = "0xce4346e22dD8288D2971416d29E99DB22385E0A4";
+const CONTRACT_ADDRESS = '0xce4346e22dD8288D2971416d29E99DB22385E0A4';
 
 /// Links ///
 
@@ -32,7 +32,7 @@ const RARIBLE_COLLECTION_LINK = `https://rinkeby.rarible.com/collection/${CONTRA
 const ETHERSCAN_LINK = `https://rinkeby.etherscan.io/address/${CONTRACT_ADDRESS}`;
 
 // Github Org Repo
-const GITHUB_REPO =  `https://github.com/Rainbow-NFT`;
+const GITHUB_REPO = `https://github.com/Rainbow-NFT`;
 
 // Token Cap
 var TOTAL_TOKEN_AMOUNT = Number(10000);
@@ -40,111 +40,117 @@ var TOTAL_TOKEN_AMOUNT = Number(10000);
 /// ============ Main App ============
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [tokenId, setTokenId] = React.useState('X');
+  const [setCurrentAccount] = useState('');
+  const [tokenId, setTokenId] = useState('X');
+  const [boolBeans, isBoolBeans] = useState(0);
 
   /// ============ API REQUEST ============
-  
+
   // Fetch tokens left
   useEffect(() => {
-    request('https://secure-shore-07844.herokuapp.com/currentTokenId', function (error, response, body) {
-    
-      if (error != null) {
-        console.log(error)
-      }
-        console.log(response)
+    request(
+      'https://secure-shore-07844.herokuapp.com/currentTokenId',
+      function (error, response, body) {
+        if (error != null) {
+          console.log(error);
+        }
+        console.log(response);
         console.log(`Tokens left: ${TOTAL_TOKEN_AMOUNT - Number(body)}`);
-  
+
         // Update tokenId
         setTokenId(TOTAL_TOKEN_AMOUNT - Number(body));
-      });
+      }
+    );
   }, []);
 
-   /// ============ Connect Wallet ============
+  /// ============ Connect Wallet ============
 
-    // Checks if the browser has Metamask
+  // Checks if the browser has Metamask
   const checkIfWalletIsConnected = () => {
-    const {ethereum} = window;
+    const { ethereum } = window;
     if (!ethereum) {
-    console.log("Metamask not installed");
-    return;
-  } else {
-    console.log("🦊 Metamask found", ethereum);
-  }
-  }
+      console.log('Metamask not installed');
+      isBoolBeans(false);
+      return;
+    } else {
+      console.log('🦊 Metamask found', ethereum);
+      isBoolBeans(true);
+    }
+  };
 
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
 
       if (!ethereum) {
-        alert("Get 🦊 Metamask!");
+        alert('Get 🦊 Metamask!');
         return;
       }
-      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
-      console.log("Connected", accounts[0]);
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log('Connected', accounts[0]);
       setCurrentAccount(accounts[0]);
 
-      /* setupEventListener() */
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   /// ============ Mint NFT ============
 
   const mintNFT = async () => {
     try {
-        const { ethereum } = window;
-        if (ethereum) {
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, SvgBitpack.abi, signer);
-            console.log("Opening wallet to confirm transaction");
-            let nftTxn = await connectedContract.mintTo(signer.getAddress());
-            console.log("⛏️ Mining in progress... please wait");
-            await nftTxn.wait();
-            console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
-            const _tokenId = await connectedContract.currentTokenId();
-            setTokenId(TOTAL_TOKEN_AMOUNT - Number(_tokenId));
-           
-        } else {
-            console.log("Ethereum object doesn't exist!");
-        }
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, SvgBitpack.abi, signer);
+        console.log('Opening wallet to confirm transaction');
+        let nftTxn = await connectedContract.mintTo(signer.getAddress());
+        console.log('⛏️ Mining in progress... please wait');
+        await nftTxn.wait();
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+        const _tokenId = await connectedContract.currentTokenId();
+        setTokenId(TOTAL_TOKEN_AMOUNT - Number(_tokenId));
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-
-  }
+  };
 
   /// ============ Render Stuff ============
 
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, [])
-  
+  }, [boolBeans]);
+
   // Render Methods
   function RenderNotConnectedContainer() {
-    return <button onClick={connectWallet} className="cta-button connect-wallet-button">
-    Connect to Wallet
-  </button>;
+    return (
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect to Wallet
+      </button>
+    );
   }
-  
+
   function RenderMintUI() {
-    return <button onClick={mintNFT} className="cta-button connect-wallet-button">
-    Mint NFT
-    </button>;
-  }  
+    return (
+      <button onClick={mintNFT} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+    );
+  }
 
   function RenderButton(props) {
     const isConnected = props.isConnected;
+
     if (isConnected) {
-      return <RenderMintUI/>;
+      return <RenderMintUI />;
     }
-    return <RenderNotConnectedContainer/>;
+    return <RenderNotConnectedContainer />;
   }
-  
-  
+
   /// ============ Return Page ============
 
   return (
@@ -152,12 +158,12 @@ const App = () => {
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">Rainbow NFTs</p>
-          <p className="sub-text">
-            Each uniquely generated, bitpacked on-chain.
-          </p>
-          <RenderButton isConnected = {true} />
+          <p className="sub-text">Each uniquely generated, bitpacked on-chain.</p>
+          <RenderButton isConnected={boolBeans} />
         </div>
-        <p className="sub-text">{tokenId}/{TOTAL_TOKEN_AMOUNT} NFTs left</p>
+        <p className="sub-text">
+          {tokenId}/{TOTAL_TOKEN_AMOUNT} NFTs left
+        </p>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
@@ -166,42 +172,28 @@ const App = () => {
             target="_blank"
             rel="noreferrer"
           >{`built by @${TWITTER_HANDLE}`}</a>
-           <a
-            className="footer-text"
-            target="_blank"
-            rel="noreferrer"
-            // just use CSS later lmao
-          >&nbsp;&nbsp;&nbsp;&nbsp;View Collection on:&nbsp;&nbsp;</a>
           <a
             className="footer-text"
             href={OPENSEA_COLLECTION_LINK}
             target="_blank"
             rel="noreferrer"
-          >{<img alt="Opensea Logo" className="opensea-logo" src={opensealogo} />}</a>
-           <a
+          >
+            {<img alt="Opensea Logo" className="opensea-logo" src={opensealogo} />}
+          </a>
+          <a
             className="footer-text"
             href={RARIBLE_COLLECTION_LINK}
             target="_blank"
             rel="noreferrer"
-          >{<img alt="Rarible Logo" className="rarible-logo" src={rariblelogo} />}</a>
-          <a
-            className="footer-text"
-            href={ETHERSCAN_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{<img alt="Etherscan Logo" className="etherscan-logo" src={etherscanlogo} />}</a>
-           <a
-            className="footer-text"
-            target="_blank"
-            rel="noreferrer"
-            // This is why I don't do much frontend
-          >&nbsp;&nbsp;&nbsp;&nbsp;Repo:&nbsp;&nbsp;</a>
-           <a
-            className="footer-text"
-            href={GITHUB_REPO}
-            target="_blank"
-            rel="noreferrer"
-          >{<img alt="Github Logo" className="github-logo" src={githublogo} />}</a>
+          >
+            {<img alt="Rarible Logo" className="rarible-logo" src={rariblelogo} />}
+          </a>
+          <a className="footer-text" href={ETHERSCAN_LINK} target="_blank" rel="noreferrer">
+            {<img alt="Etherscan Logo" className="etherscan-logo" src={etherscanlogo} />}
+          </a>
+          <a className="footer-text" href={GITHUB_REPO} target="_blank" rel="noreferrer">
+            {<img alt="Github Logo" className="github-logo" src={githublogo} />}
+          </a>
         </div>
       </div>
     </div>
